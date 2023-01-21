22751cc486ecd99e54dac02258b97eeb4dd2c019
Thanh Huyền Nguyễn <97439051+thhuyen@users.noreply.github.com>
2023-01-21 22:38:04 +0700

    Feat/base (#11)
    * fix header + detail
    
    * all footer + header + detail screen
    
    done all
    
    Co-authored-by: Mancitiss <37582541+Mancitiss@users.noreply.github.com>

--- src/js/dropdown_searchpage.js
+++ src/js/dropdown_searchpage.js
@@ -1,12 +1,12 @@
-function hamDropdown() {
-    document.querySelector(".noidung_dropdown").classList.toggle("hienThi");
-}
+// function hamDropdown() {
+//     document.querySelector(".noidung_dropdown").classList.toggle("hienThi");
+// }
 
-window.onclick = function (e) {
-    if (!e.target.matches('.nut_dropdown')) {
-        var noiDungDropdown = document.querySelector(".noidung_dropdown");
-        if (noiDungDropdown.classList.contains('hienThi')) {
-            noiDungDropdown.classList.remove('hienThi');
-        }
-    }
-}
+// window.onclick = function (e) {
+//     if (!e.target.matches('.nut_dropdown')) {
+//         var noiDungDropdown = document.querySelector(".noidung_dropdown");
+//         if (noiDungDropdown.classList.add('hienThi')) {
+//             noiDungDropdown.classList.remove('hienThi');
+//         }
+//     }
+// }
